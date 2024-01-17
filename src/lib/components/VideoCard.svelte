fb4bfc11c10ae0e015165f4425cf83a07457da92
Radu <45122923+raduElisei@users.noreply.github.com>
2024-01-17 15:58:26 +0200

    Add Video Card

--- src/lib/components/VideoCard.svelte
+++ src/lib/components/VideoCard.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
-
 </script>
 
-<div>
-    <div>
-        
-    </div>
-</div>
+<a href="/">
+	<div class="relative">
+		<div
+			class="absolute bg-black/80 text-white bottom-1 px-1 text-sx right-1 font-semibold rounded"
+		>
+			10:21
+		</div>
+		<img
+			src="/thumbnail.webp"
+			class="sm:h-[208px] h-[250px] w-full object-cover rounded-2xl bg-gray-200"
+			alt=""
+		/>
+	</div>
+	<div class="flex gap-3 mt-3">
+		<img src="/avatar.png" class="h-9 w-9 rounded-full bg-gray-100" alt="" />
+		<div>
+			<h3 class="capitalize font-medium">Why Svelte is BETTER Than React | Prime Reacts</h3>
+			<p class="text-gray-600 text-sm capitalize mt-1">Primeagen</p>
+			<div class="text-gray-600 text-sm">
+				<span>3 mil</span>
+				<span>2 hrs ago</span>
+			</div>
+		</div>
+	</div>
+</a>
